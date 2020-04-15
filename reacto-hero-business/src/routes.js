fcbb21b5d0dce9beb882603cebfad7f206852700
Mathews Motta <mathewscsmotta@gmail.com>
2020-04-15 01:18:10 +0100

    added validation

--- reacto-hero-business/src/routes.js
+++ reacto-hero-business/src/routes.js
@@ -1,4 +1,5 @@
  const express = require('express');
+ const { celebrate, Segments, Joi } = require('celebrate');
  
  const NgoController = require('./controllers/NgoController')
  const OccurrenceController = require('./controllers/OccurrenceController')
@@ -7,15 +8,47 @@
 
  const routes = express.Router();
 
-routes.post('/sessions', SessionController.create)
+routes.post('/sessions', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        id: Joi.string().required(),
+    }),}), SessionController.create)
 
  routes.get('/ngos', NgoController.index);
- routes.post('/ngos', NgoController.create);
 
- routes.get('/occurrences', OccurrenceController.index);
- routes.post('/occurrences', OccurrenceController.create);
- routes.delete('/occurrences/:id', OccurrenceController.delete);
+ routes.post('/ngos', celebrate({
+     [Segments.BODY]: Joi.object().keys({
+         name: Joi.string().required(),
+         email: Joi.string().required().email(),
+         whatsapp: Joi.number().required().min(10).max(12),
+         city: Joi.string().required(),
+         country: Joi.string().required(),
+     })
+ }), NgoController.create);
 
- routes.get('/ngos/query', NgoQueryController.index);
+ routes.get('/occurrences', celebrate({
+     [Segments.QUERY]: Joi.object().keys({
+         page: Joi.number(),
+     })
+ }), OccurrenceController.index);
+ routes.post('/occurrences', celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        title: Joi.string().required(),
+        description: Joi.string(),
+        value: Joi.number().required(),
+    }),
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required(),
+     }).unknown()
+}), OccurrenceController.create);
+ routes.delete('/occurrences/:id', celebrate({
+     [Segments.PARAMS]: Joi.object().keys({
+         id: Joi.number().required(),
+     })
+ }), OccurrenceController.delete);
+
+ routes.get('/ngos/query',  celebrate({
+    [Segments.HEADERS]: Joi.object({
+        authorization: Joi.string().required(),
+     }).unknown()}), NgoQueryController.index);
 
  module.exports = routes;
