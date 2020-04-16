c4f96b6bb89167444725b0a5aa6e4fc85b81f8d9
Mathews Motta <mathewscsmotta@gmail.com>
2020-04-16 22:05:24 +0100

    Added test to delete occurrence

--- reacto-hero-business/tests/integration/occurrence.spec.js
+++ reacto-hero-business/tests/integration/occurrence.spec.js
@@ -32,6 +32,25 @@ describe('occurrence', () => {
         expect(responseOc.body).toHaveProperty('id');
     });
 
+    it('should be able to delete a occurrence', async () => {
+        const responseNgo = await request(app).post('/ngos').send({
+                name:"IMVF",
+                email:"info@test.pt",
+                whatsapp:"351213256300",
+                city:"Lisboa",
+                country:"Portugal"
+            });
+        
+        const responseOcCreate = await request(app).post('/occurrences').send({
+            title:"SomeOcc",
+            description:"desc",
+            value:120,
+        }).set('authorization',responseNgo.body.id);
+
+        const response = await request(app).delete(`/occurrences/${responseOcCreate.body.id}`).send().set('authorization',responseNgo.body.id);
+        expect(response.status).toBe(204);
+    });
+
     it('should be able to list occurrences', async () => {
         const response = await request(app).get('/occurrences').send();
         
