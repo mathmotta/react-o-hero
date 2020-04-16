f55d59552eb3cb1b1325546f1491d612df80490b
Mathews Motta <mathewscsmotta@gmail.com>
2020-04-16 22:00:39 +0100

    Added tests to occurrence

---
+++ reacto-hero-business/tests/integration/occurrence.spec.js
@@ -0,0 +1,40 @@
+const request = require('supertest');
+const app = require('../../src/app');
+const conn = require('../../src/db/connection');
+
+describe('occurrence', () => {
+    beforeEach(async () => {
+        await conn.migrate.latest();
+    });
+
+    afterAll(async () => {
+        await conn.destroy()
+        await conn.migrate.rollback();
+    })
+
+    it('should be able to create a new occurrence', async () => {
+        const responseNgo = await request(app).post('/ngos').send({
+                name:"IMVF",
+                email:"info@test.pt",
+                whatsapp:"351213256300",
+                city:"Lisboa",
+                country:"Portugal"
+            });
+        
+        expect(responseNgo.body.id).toHaveLength(8);
+
+        const responseOc = await request(app).post('/occurrences').send({
+            title:"SomeOcc",
+            description:"desc",
+            value:120,
+        }).set('authorization',responseNgo.body.id);
+
+        expect(responseOc.body).toHaveProperty('id');
+    });
+
+    it('should be able to list occurrences', async () => {
+        const response = await request(app).get('/occurrences').send();
+        
+        expect(response.body.length).toBeGreaterThan(0);
+    });
+});
